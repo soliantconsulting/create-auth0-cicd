31c0dfa688003b0c52de4f46d5af563d6f020d78
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2024-05-17 17:26:39 +0200

    fix(skeleton): return value instead of name from loadFromEnv

--- skeleton/base/src/env-variables.ts
+++ skeleton/base/src/env-variables.ts
@@ -1,12 +1,15 @@
 const loadFromEnv = (name: string): string => {
-    if (!process.env[name]) {
+    const value = process.env[name];
+
+    if (!value) {
         throw new Error(`Env variable with name ${name} not found`);
     }
 
-    return name;
+    return value;
 };
 
 export const envVariables = {
+    auth0Domain: loadFromEnv("AUTH0_DOMAIN"),
     ses: {
         accessKeyId: loadFromEnv("SES_ACCESS_KEY_ID"),
         secretAccessKey: loadFromEnv("SES_SECRET_ACCESS_KEY"),
